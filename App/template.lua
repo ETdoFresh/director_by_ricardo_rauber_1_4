bab30ed068a8fe9aa215650815b988e0116cdc0e
E.T <ETdoFresh@gmail.com>
2011-12-14 23:26:09 -0600

    I modded ricardo's director for use without modules. However, probably use storyboard at this point.

---
+++ App/template.lua
@@ -0,0 +1,42 @@
+--====================================================================--
+-- SCENE: [NAME]
+--====================================================================--
+
+--[[
+
+ - Version: [1.0]
+ - Made by: [name]
+ - Website: [url]
+ - Mail: [mail]
+
+******************
+ - INFORMATION
+******************
+
+  - [Your info here]
+
+--]]
+local template = {}
+function template.new ()
+	
+	------------------
+	-- Groups
+	------------------
+	
+	local localGroup = display.newGroup()
+	
+	------------------
+	-- Your code here
+	------------------
+	
+	--- CODE ---
+	
+	------------------
+	-- MUST return a display.newGroup()
+	------------------
+	
+	return localGroup
+	
+end
+
+return template
